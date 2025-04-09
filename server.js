a5a226c4d0289934fa5510ed0f9c4a35b0686a09
Matt <mchurchman@hotmail.com>
2025-04-08 22:24:02 -0600

    replacing bcrypt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const { Pool } = require('pg'); // PostgreSQL client
 
